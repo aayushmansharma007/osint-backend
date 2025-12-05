11714a402ab3e78fce7d84f1ef2ec67b4294d341
aayushmansharma007 <aayushman2006lanos@gmail.com>
2025-12-05 20:34:15 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
+# In-memory storage for users
 users = {}
 
 @app.route("/")
@@ -29,3 +32,10 @@ def login():
     if users.get(username) == password:
         return jsonify({"message": "Login successful!"})
     return jsonify({"error": "Invalid credentials"}), 404
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    """Endpoint to get all registered users"""
+    # Convert the users dictionary to a list of usernames
+    user_list = list(users.keys())
+    return jsonify({"users": user_list, "count": len(user_list)})
